3c26e44379617c573f670b8a1025c519f1da7d05
santa <s.yushmanov777@gmail.com>
2026-06-04 10:28:31 +0700

    refactor proxy image component

--- src/components/common/proxy-image.tsx
+++ src/components/common/proxy-image.tsx
@@ -3,6 +3,6 @@ import type { ComponentPropsWithoutRef } from 'react';
 interface Props extends ComponentPropsWithoutRef<'img'> {}
 
 export const ProxyImage = (props: Props) => {
-  const { src, ...rest } = props;
-  return <img {...rest} src={`/api/image-proxy?url=${encodeURIComponent(src)}`} loading="lazy" />;
+  const { src = '', alt, ...rest } = props;
+  return <img {...rest} src={`/api/image-proxy?url=${encodeURIComponent(src as string)}`} alt={alt} loading="lazy" />;
 };
