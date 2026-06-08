2df47650b7219c387266169c81d6a69fe46c89d5
santa <s.yushmanov777@gmail.com>
2026-06-08 11:55:31 +0700

    refactor classes

--- src/components/common/textarea.tsx
+++ src/components/common/textarea.tsx
@@ -4,18 +4,22 @@ interface Props extends ComponentPropsWithoutRef<'textarea'> {
   labelText?: string;
   className?: string;
   noMargin?: boolean;
+  isError?: boolean;
+  message?: string;
 }
 
 export const Textarea = (props: Props) => {
-  const { labelText, className, noMargin, ...rest } = props;
+  const { labelText, className, noMargin, isError, message, ...rest } = props;
+  const isErrorClass = isError ? ' border-red-400' : '';
   return (
     <div className={`mb-${noMargin ? '0' : '4'} relative${className ? ` ${className}` : ''}`}>
       {labelText && <label className="block mb-2 font-medium text-gray-700">{labelText}</label>}
       <textarea
         {...rest}
-        className={`w-full px-4 py-2 bg-gray-50 border-2 border-gray-200 rounded-xl focus:ring-2 focus:ring-orange-300 focus:border-orange-400 transition-all outline-none resize-none`}
+        className={`w-full px-4 py-2 bg-gray-50 border-2 border-gray-200 rounded-xl focus:ring-2 focus:ring-orange-300 focus:border-orange-400 transition-all outline-none resize-none${isErrorClass}`}
         rows={3}
       ></textarea>
+      {message && <div className="text-red-400 text-right">{message}</div>}
     </div>
   );
 };
