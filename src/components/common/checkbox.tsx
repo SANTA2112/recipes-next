5624109c68376c9ca21579244462651fe6932f88
santa <s.yushmanov777@gmail.com>
2026-06-08 11:55:20 +0700

    added other props

--- src/components/common/checkbox.tsx
+++ src/components/common/checkbox.tsx
@@ -10,18 +10,17 @@ interface Props {
 }
 
 export const Checkbox = (props: Props) => {
-  const { checked, onChange, className, disabled, label, name } = props;
+  const { onChange, className, disabled, label, ...rest } = props;
 
   return (
     <label className={`inline-flex items-center gap-3 select-none cursor-pointer${className ? ` ${className}` : ''}`}>
       <div className="relative h-5 w-5">
         <input
+          {...rest}
           type="checkbox"
-          checked={checked}
           onChange={(e) => onChange?.(e.target.checked)}
           disabled={disabled}
           className="peer hidden"
-          name={name}
         />
         <div className="absolute inset-0 rounded-md border transition-all duration-200 border-zinc-400 bg-white peer-checked:border-orange-600 peer-checked:bg-orange-600 peer-focus-visible:ring-2 peer-focus-visible:ring-orange-500 peer-focus-visible:ring-offset-2 peer-disabled:bg-zinc-100"></div>
         <CheckedIcon className="absolute inset-0.5 opacity-0 peer-checked:opacity-100 transition-all duration-200" />
