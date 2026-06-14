dac2988c103987d2eab231fe658de032676fa736
santa <s.yushmanov777@gmail.com>
2026-06-14 18:07:15 +0700

    redirect then already auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,18 @@
+'use client';
+import { redirect } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
 import { Wrapper } from '@/components/common/wrapper';
 import { Form } from '@/components/ui/authForm/form';
 import { FormWrapper } from '@/components/ui/authForm/wrapper';
+import { ROUTES } from '@/constants';
 
 const LoginPage = () => {
+  const { status } = useSession();
+  if (status === 'authenticated') {
+    redirect(ROUTES.recipes);
+  }
+
   return (
     <Wrapper className="flex items-center justify-center">
       <FormWrapper type="login">
