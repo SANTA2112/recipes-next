8b8ccdfb70d33d305a743fea2963165830f4dbb1
santa <s.yushmanov777@gmail.com>
2026-06-19 14:23:49 +0700

    show my-recipes

--- src/app/my-recipes/page.tsx
+++ src/app/my-recipes/page.tsx
@@ -1,37 +1,28 @@
-import Link from 'next/link';
-
+import { getUserRecipes } from '@/actions/recipe';
 import PlusIcon from '@/assets/icons/plus.svg';
 import { Button } from '@/components/common/buttons/button';
+import { ErrorMessage } from '@/components/common/error-message';
 import { Heading } from '@/components/common/heading';
 import { Wrapper } from '@/components/common/wrapper';
 import { Recipe } from '@/components/ui/cards/recipe';
 import { ROUTES } from '@/constants';
 
-const MyRecipesPage = () => {
-  const recipes = Array.from({ length: 10 }, (_, i) => i + 1);
+const MyRecipesPage = async () => {
+  const { error, recipes } = await getUserRecipes();
   return (
     <Wrapper>
       <div className="mb-6 flex flex-col items-end">
         <Heading>Мои рецепты</Heading>
-        <Link href={ROUTES.newRecipe}>
+        <a href={ROUTES.newRecipe}>
           <Button notFullWidth>
             <PlusIcon className="w-5 h-5" />
             <span>Создать новый рецепт</span>
           </Button>
-        </Link>
+        </a>
       </div>
+      {recipes?.length === 0 && <div className="flex justify-center items-center text-2xl">Список рецептов пуст</div>}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {recipes.map((item) => (
-          <Recipe
-            key={item}
-            title="Борщ классический"
-            shortDesc="Традиционный украинский борщ с говядиной и сметаной"
-            cookTime={90}
-            servings={1}
-            image="https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?w=800"
-            slug={item.toString()}
-          />
-        ))}
+        {recipes ? recipes.map((item) => <Recipe key={item.id} {...item} />) : <ErrorMessage>{error}</ErrorMessage>}
       </div>
     </Wrapper>
   );
