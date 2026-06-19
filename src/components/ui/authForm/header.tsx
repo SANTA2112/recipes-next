158fa1a95462d43a9c57fd500119ddd498bde9cc
santa <s.yushmanov777@gmail.com>
2026-06-19 14:21:58 +0700

    added animation prop

--- src/components/ui/authForm/header.tsx
+++ src/components/ui/authForm/header.tsx
@@ -3,14 +3,17 @@ import Image from 'next/image';
 interface Props {
   title: string;
   description: string;
+  withAnimation?: boolean;
 }
 
 export const Header = (props: Props) => {
-  const { description, title } = props;
+  const { description, title, withAnimation } = props;
   return (
     <div className="text-center mb-4">
       <div className="flex justify-center mb-4">
-        <div className="w-20 h-20 bg-linear-to-br from-orange-400 to-red-500 rounded-3xl flex items-center justify-center shadow-lg">
+        <div
+          className={`w-20 h-20 bg-linear-to-br from-orange-400 to-red-500 rounded-3xl flex items-center justify-center shadow-lg${withAnimation ? ' animate-bounce' : ''}`}
+        >
           <Image src="/logo.svg" alt="Русская кухня" width={48} height={48} />
         </div>
       </div>
