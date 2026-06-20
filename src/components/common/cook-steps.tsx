f04550bfd132af796e7c71a419722613272dffcb
santa <s.yushmanov777@gmail.com>
2026-06-20 15:24:38 +0700

    fix props by new data

--- src/components/common/cook-steps.tsx
+++ src/components/common/cook-steps.tsx
@@ -1,6 +1,6 @@
-interface Props {
-  instructions: string[];
-}
+import type { Recipe } from '@/constants/form-state';
+
+interface Props extends Pick<Recipe, 'instructions'> {}
 
 export const CookSteps = (props: Props) => {
   const { instructions } = props;
@@ -10,12 +10,12 @@ export const CookSteps = (props: Props) => {
       <div className="bg-white rounded-2xl p-6 shadow-sm border-2 border-gray-100">
         <h2 className="text-2xl mb-6 font-semibold text-gray-800">Приготовление</h2>
         <ol className="space-y-5">
-          {instructions.map((desc, i) => (
+          {instructions.map(({ value }, i) => (
             <li key={i} className="flex gap-4">
               <span className="shrink-0 w-10 h-10 bg-linear-to-br from-orange-400 to-red-500 text-white rounded-2xl flex items-center justify-center font-bold shadow-md">
                 {i + 1}
               </span>
-              <p className="text-gray-700 pt-2 leading-relaxed">{desc}</p>
+              <p className="text-gray-700 pt-2 leading-relaxed">{value}</p>
             </li>
           ))}
         </ol>
