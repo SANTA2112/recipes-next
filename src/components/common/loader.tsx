71dab7773624efc96ae0a27e144f6c3c9c9852f2
santa <s.yushmanov777@gmail.com>
2026-06-22 13:11:39 +0700

    fix title

--- src/components/common/loader.tsx
+++ src/components/common/loader.tsx
@@ -3,7 +3,7 @@ import { Header } from '@/components/ui/authForm/header';
 export const Loader = () => {
   return (
     <div className="fixed inset-0 bg-red flex items-center justify-center z-50 select-none">
-      <Header title="Пожалуйста подождите " description="Идет обработка данных" withAnimation />
+      <Header title="Пожалуйста подождите" description="Идет обработка данных" withAnimation />
     </div>
   );
 };
