77c44b9ab3242d95c6c8c38216cfdb246d467bd9
santa <s.yushmanov777@gmail.com>
2026-06-22 13:11:33 +0700

    fix header size

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -10,13 +10,15 @@ import { menu, ROUTES } from '@/constants';
 
 export const Header = () => {
   const { data: session, status } = useSession();
+
   const handleSignOut = async () => {
     await signOutFunc();
   };
   const isAuth = status === 'authenticated';
+  const isLoading = status === 'loading';
   return (
-    <header className="bg-white/80 backdrop-blur-md sticky top-0 z-50 shadow-sm">
-      <div className="container mx-auto px-4 py-4 flex flex-col md:flex-row items-center justify-between">
+    <header className="bg-white/80 backdrop-blur-md sticky top-0 z-50 shadow-sm min-h-20 flex">
+      <div className="container mx-auto px-4 py-4 flex flex-col gap-2 md:gap-0 md:flex-row items-center justify-between">
         <StyledLink href={ROUTES.recipes} className="flex items-center gap-4 group">
           <div className="w-10 h-10 bg-linear-to-br from-orange-400 to-red-500 rounded-2xl flex items-center justify-center shadow-md group-hover:shadow-lg transition-shadow">
             <Image src="/logo.svg" width={24} height={24} alt="Русская кухня" priority />
@@ -25,47 +27,56 @@ export const Header = () => {
             Русская кухня
           </span>
         </StyledLink>
-        <nav className="flex items-center gap-4">
-          {menu
-            .filter((item) => (item.link === ROUTES.myrecipes ? isAuth : true))
-            .map(({ link, title }) => (
-              <StyledLink
-                key={link}
-                href={link}
-                className="text-gray-700 hover:text-orange-600 transition-colors font-medium"
+        {isLoading ? (
+          <p className="text-gray-700 font-medium">Загрузка меню...</p>
+        ) : (
+          <nav className="flex items-center gap-4">
+            {menu
+              .filter((item) => (item.link === ROUTES.myrecipes ? isAuth : true))
+              .map(({ link, title }) => (
+                <StyledLink
+                  key={link}
+                  href={link}
+                  className="text-gray-700 hover:text-orange-600 transition-colors font-medium"
+                >
+                  {title}
+                </StyledLink>
+              ))}
+          </nav>
+        )}
+
+        {isLoading ? (
+          <p className="text-gray-700 font-medium">Загрузка сессии...</p>
+        ) : (
+          <div className="flex items-center gap-4">
+            {isAuth && <p className="text-orange-600 font-medium">{session.user?.email}</p>}
+            {isAuth ? (
+              <button
+                className="text-gray-700 hover:text-orange-600 transition-colors font-medium cursor-pointer"
+                onClick={handleSignOut}
               >
-                {title}
-              </StyledLink>
-            ))}
-        </nav>
-        <div className="flex items-center gap-4">
-          {isAuth && <p className="text-orange-600 font-medium">{session.user?.email}</p>}
-          {isAuth ? (
-            <button
-              className="text-gray-700 hover:text-orange-600 transition-colors font-medium cursor-pointer"
-              onClick={handleSignOut}
-            >
-              Выйти
-            </button>
-          ) : (
-            <>
-              <StyledLink
-                className="text-gray-700 hover:text-orange-600 transition-colors font-medium"
-                href={ROUTES.login}
-              >
-                Вход
-              </StyledLink>
-              <StyledLink href={ROUTES.register}>
-                <Button>
-                  <div className="flex items-center justify-center gap-2">
-                    <ClientIcon className="w-4 h-4 text-white" />
-                    <span>Регистрация</span>
-                  </div>
-                </Button>
-              </StyledLink>
-            </>
-          )}
-        </div>
+                Выйти
+              </button>
+            ) : (
+              <>
+                <StyledLink
+                  className="text-gray-700 hover:text-orange-600 transition-colors font-medium"
+                  href={ROUTES.login}
+                >
+                  Вход
+                </StyledLink>
+                <StyledLink href={ROUTES.register}>
+                  <Button>
+                    <div className="flex items-center justify-center gap-2">
+                      <ClientIcon className="w-4 h-4 text-white" />
+                      <span>Регистрация</span>
+                    </div>
+                  </Button>
+                </StyledLink>
+              </>
+            )}
+          </div>
+        )}
       </div>
     </header>
   );
